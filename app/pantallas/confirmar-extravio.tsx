40ade871d986aafc4daf66931d28f9c39e5564d5
Agustin <agussebastiandominguez@gmail.com>
2025-01-29 10:19:58 -0300

    Reorganizacion de proyecto

---
+++ app/pantallas/confirmar-extravio.tsx
@@ -0,0 +1,67 @@
+import React, {useState} from "react"
+import { StyleSheet, ScrollView, View } from "react-native";
+import { useTheme } from 'react-native-paper';
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormularioConfirmarExtravio from "../componentes/formularios/formularioConfirmarExtravio";
+import AppbarNav from "../componentes/navegacion/appbarNav";
+
+// TODO: Request POST para la creación de un anuncio de animal extraviado
+export default function ConfirmarExtravio() {
+    const theme = useTheme();
+    const [edicion, setEdicion] = useState(false);
+    const [datosFamiliar, setDatosFamiliar] = useState({
+      nombre: 'Chili',
+      especie: 'Perro',
+      raza: 'callejero',
+      tamanio: 'grande',
+      colores: 'tricolor',
+      fechanac: '14/04/2021',
+      observaciones: 'compañero y sociable',
+      sexo: 'macho',
+      esterilizado: true,
+      identificado: false,
+      domicilio: 'Puerto español 844'
+    });
+    const navigation = useNavigation();
+    
+    return (
+        <>
+            <AppbarNav titulo="Confirmar datos" />
+            <SafeAreaView style={{ alignItems: "center",flex:1}}>
+            <View style={{marginHorizontal:10,width: "100%"}}>
+                <ScrollView contentContainerStyle={ {...styles.containerScroll}}>
+                    <FormularioConfirmarExtravio data={datosFamiliar} onSumbit={setEdicion}  />
+                </ScrollView>
+            </View>
+            </SafeAreaView>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        flex:1
+    },
+    containerScroll: {
+        paddingBottom: 20,
+        paddingHorizontal: '5%'
+    },
+    input:{
+    marginBottom: 16,
+    },
+    fotoFamiliar: {
+        marginTop: 35,
+    },
+    nombreFamiliar: {
+        // borderBottomWidth: 1,
+        // borderEndColor: 'black',
+        textAlign: 'center',
+        width: 150,
+        padding: 15,
+    },
+    cardFamiliar: {
+    marginVertical: 5,
+    },
+});
---
+++ app/pantallas/extraviado.tsx
@@ -0,0 +1,75 @@
+import {useState} from "react"
+import { StyleSheet, ScrollView, View } from "react-native";
+import { useTheme } from 'react-native-paper';
+import CardFamiliar from "../componentes/cards/cardFamiliar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AppbarNav from "../componentes/navegacion/appbarNav";
+
+export default function Extravio() {
+    const theme = useTheme();
+    const [datosFamiliar, setDatosFamiliar] = useState({
+      nombre: 'Chili',
+      especie: 'Perro',
+      raza: 'callejero',
+      tamanio: 'grande',
+      colores: 'tricolor',
+      fechanac: '14/04/2021',
+      observaciones: 'compañero y sociable',
+      sexo: 'macho',
+      esterilizado: true,
+      identificado: false,
+      domicilio: 'Puerto español 844'
+    });
+    
+    return (
+        <>
+            <AppbarNav titulo="Seleccione un familiar" />     
+            
+            <SafeAreaView style={{ alignItems: "center",flex:1,backgroundColor: theme.colors.surface}}>
+                <View style={{marginHorizontal:10}}>
+                    <ScrollView
+                        style={ styles.scrollView }
+                        contentContainerStyle={{ alignItems: "center" }}
+                    >
+                        <CardFamiliar style={ styles.cardFamiliar } navigateTo="ConfirmarExtravio" data={{nombre: 'Chili', especie: 'Canino'}} />
+                        <CardFamiliar style={ styles.cardFamiliar } navigateTo="ConfirmarExtravio" data={{nombre: 'Duque', especie: 'Canino'}} />
+                        <CardFamiliar style={ styles.cardFamiliar } navigateTo="ConfirmarExtravio" data={{nombre: 'Draco', especie: 'Canino'}} />
+                        <CardFamiliar style={ styles.cardFamiliar } navigateTo="ConfirmarExtravio" data={{nombre: 'Sur', especie: 'Felino'}} />
+                    </ScrollView>
+                </View>
+            </SafeAreaView>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        flex:1
+    },
+    scrollView: {
+        marginTop:5,
+    
+    },
+    containerScroll: {
+    paddingBottom: 20,
+    borderRadius: 10,
+    margin: 12,
+    },
+    input:{
+    marginBottom: 16,
+    },
+    fotoFamiliar: {
+        marginTop: 35,
+    },
+    nombreFamiliar: {
+        // borderBottomWidth: 1,
+        // borderEndColor: 'black',
+        textAlign: 'center',
+        width: 150,
+        padding: 15,
+    },
+    cardFamiliar: {
+    marginVertical: 5,
+    },
+});

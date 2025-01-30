24d680d6404201d339c01efce12b1ed8f6069224
Agustin <agussebastiandominguez@gmail.com>
2025-01-30 13:39:26 -0300

    Nuevo banner de información

---
+++ app/componentes/bannerInfo.tsx
@@ -0,0 +1,16 @@
+import { Banner, Text, useTheme } from "react-native-paper";
+
+type Props = {
+    texto: string
+}
+
+export default function BannerInfo({texto}: Props) {
+
+    const theme = useTheme()
+
+    return (
+        <Banner icon="information"  visible style={{margin: 10, borderRadius: 20,backgroundColor: theme.colors.tertiaryContainer}}>
+            <Text style={{textAlign: 'center',color:theme.colors.onTertiaryContainer}}>{texto}</Text>
+        </Banner>
+    )
+}

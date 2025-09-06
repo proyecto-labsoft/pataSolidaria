a2130ecf4678edc294215131e38d6d48770bee90
JoseGarayN <jose.garay@redb.ee>
2025-09-06 17:33:40 -0300

    feat: se agregan Customs Exceptions

---
+++ api/src/main/java/mascotas/project/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package mascotas.project.exceptions;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}

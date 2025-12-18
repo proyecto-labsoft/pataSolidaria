357a46a585cde9e63b3f631652038cb948c8fab6
Ezequiel Saizar <esaizar88@gmail.com>
2025-12-18 01:49:52 -0300

    subo url de api en el servidor para generar apk

--- app/api/urlApi.js
+++ app/api/urlApi.js
@@ -5,6 +5,6 @@
 // const URL_API = "http://192.168.1.127:8083"
 // const URL_API = "http://192.168.1.14:8083"  // eze
 // const URL_API = "http://192.168.49.54"; // muni eze
-// const URL_API = "https://patasolidaria-production.up.railway.app"; // Producción
-const URL_API = "http://192.168.1.17:8083";
+ const URL_API = "https://patasolidaria-production.up.railway.app"; // Producción
+//const URL_API = "http://192.168.1.17:8083";
 export default URL_API;
